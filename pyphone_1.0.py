f4f62c321abe2eba80eaa4b95489a05d92bf74ba
joaosantosmedeiros <jopesame@gmail.com>
2023-06-21 09:33:33 -0300

    fix version 1.0

--- pyphone_1.0.py
+++ pyphone_1.0.py
@@ -1,7 +1,30 @@
-print("=====================================")
-print("==== M E N U   P R I N C I P A L ====")
-print("=====================================")
-print('     1 - Cadastro de usuários')
-print('     2 - Cadastro de funcionários')
-print('     3 - Cadastro de produtos')
-print('     0 - Finalizar')
+from os import system
+
+while True:
+    print("=====================================")
+    print("==== M E N U   P R I N C I P A L ====")
+    print("=====================================")
+    print('     1 - Cadastro de usuários')
+    print('     2 - Cadastro de funcionários')
+    print('     3 - Cadastro de Celulares')
+    print('     0 - Finalizar')
+    print("=====================================")
+
+    opc = input('\nEscolha sua opção: ')
+    if opc not in '1230':
+        print('=== Opção Invalida ===')
+    if opc == '0':
+        break
+    elif opc == '1':
+        print('====      Usuários      ====')
+        print('==== Em desenvolvimento ====')
+    elif opc == '2':
+        print('====    Funcionários    ====')
+        print('==== Em desenvolvimento ====')
+    elif opc == '3':
+        print('====     Celulares      ====')
+        print('==== Em desenvolvimento ====')
+
+    input("Pressione qualquer tecla para continuar")
+    system('cls || clear')
+print('Fim.')
