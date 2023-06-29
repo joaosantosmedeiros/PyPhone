f6eccc96e04a18945c25cd48cf2461c0a6467ae1
joaosantosmedeiros <jopesame@gmail.com>
2023-06-29 15:46:51 -0300

    chore: organização de código

--- pyphone_4_0.py
+++ pyphone_4_0.py
@@ -50,15 +50,19 @@ while True:
         while True:
             menu_secundario('clientes')
             option = input('\nEscolha sua opção: ')
+
             if option == '0':
                 break
+
             elif option == '1':
                 username = input('\nDigite o nome do cliente: ')
                 email = input('Digite o email do cliente: ')
                 password = input('Digite a senha do cliente: ')
                 print('\n', Clients.createClient(username=username, email=email, password=password, list=clients), '\n')
+
             elif option == '2':
                 print(Clients.readAllClients(list=clients))
+
             elif option == '3':
                 email = input('Digite o email: ')
                 result = (Clients.readOneClient(email=email, list=clients))
@@ -68,6 +72,7 @@ while True:
                     print(f'\nUsername: {result["value"][0]}')
                     print(f'Email: {result["value"][1]}\n')
                     # Não é mostrado a senha por questões de segurança
+            
             elif option == '4':
                 email = input('Digite o email a ser buscado: ')
                 result = Clients.readOneClient(email=email, list=clients)
@@ -81,6 +86,7 @@ while True:
                     updated_result = Clients.updateClient(
                         username=username, email=email, password=password, list=clients, list_index=result['index'])
                     print('\n', updated_result, '\n')
+            
             elif option == '5':
                 email = input('Insira o email: ')
                 print('\n', Clients.deleteClient(self=Clients, email=email, list=clients), '\n')
@@ -90,16 +96,20 @@ while True:
         while True:
             menu_secundario('funcionários')
             option = input('\nEscolha sua opção: ')
+
             if option == '0':
                 break
+            
             elif option == '1':
                 username = input('\nDigite o nome do funcionário: ')
                 email = input('Digite o email do funcionário: ')
                 contact = input('Digite o contato do funcionário: ')
                 password = input('Digite a senha do funcionário: ')
                 print('\n', Employees.createEmployee(username=username, email=email, contact=contact, password=password, list=employees), '\n')
+            
             elif option == '2':
                 print(Employees.readAllEmployees(list=employees))
+            
             elif option == '3':
                 email = input('Digite o email: ')
                 result = (Employees.readOneEmployee(email=email, list=employees))
@@ -110,6 +120,7 @@ while True:
                     print(f'Email: {result["value"][1]}')
                     print(f'Contato: {result["value"][2]}\n')
                     # Não é mostrado a senha por questões de segurança
+            
             elif option == '4':
                 email = input('Digite o email a ser buscado: ')
                 result = Employees.readOneEmployee(email=email, list=employees)
@@ -123,6 +134,7 @@ while True:
 
                     updated_result = Employees.updateEmployee( username=username, email=email, contact=contact, password=password, list=employees, list_index=result['index'])
                     print('\n', updated_result, '\n')
+            
             elif option == '5':
                 email = input('Insira o email: ')
                 print('\n', Employees.deleteEmployee(self=Employees, email=email, list=employees), '\n')
@@ -132,17 +144,22 @@ while True:
         while True:
             menu_secundario('celulares')
             option = input('\nEscolha sua opção: ')
+            
             if option == '0':
                 break
+            
             elif option == '1':
                 name = input('\nDigite o nome do celular:')
                 price = input('Digite o preço do celular: ')
                 print('\n', Cellphones.createCellphone(name=name, price=price, list=cellphones), '\n')
+            
             elif option == '2':
                 print(f'\n{Cellphones.readAllCellphones(cellphones)}\n')
+            
             elif option == '3':
                 id = input('Digite o id do celular: ')
                 print(f'\n{Cellphones.readOneCellphone(id=id, list=cellphones)}\n')
+            
             elif option == '4':
                 id = input('Digite o id: ')
                 result = Cellphones.getKey(list=cellphones, id=id)
@@ -152,6 +169,7 @@ while True:
                     name = input('Digite o nome: ')
                     price = input('Digite o preço: ')
                     print(f'\n{Cellphones.updateCellphone(self=Cellphones, id=id, name=name, price=price, list=cellphones)}\n')
+            
             elif option == '5':
                 id = input('Digite o id do celular: ')
                 print(f'\n{Cellphones.deleteCellphone(self=Cellphones, id=id, list=cellphones)}\n')
